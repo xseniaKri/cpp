16fdaa4c9ff1277343ec64b051e879ac8dc63294
xsenia-kri <Marta@DESKTOP-H14FJ83>
2025-02-06 15:00:29 +0300

    incorrect work

--- lab6_2/main.cpp
+++ lab6_2/main.cpp
@@ -1,66 +1,51 @@
 #include "myStack.h"
 #include <iostream>
+#include <string>
 using namespace std;
 
 //проверяем что перед нами открывающая скобка
 bool open_bracket(char target) {
-    char arr[] = {'{', '(', '['};
-    int i = 0;
-    while (i < 3) {
-        if (target == arr[i]) {
-            cout << "Found it!";
-            return true;
-            break;
-        }
-        i++;
-    }
-    return false;
+    return (target == '{' || target == '[' || target == '(');
 }
 
 //проверяем что перед нами закрывающая скобка
 bool close_bracket(char target) {
-    char arr[] = {'}', ')', ']'};
-    int i = 0;
-    while (i < 3) {
-        if (target == arr[i]) {
-            return true;
-            break;
-        }
-        i++;
-    }
-    return false;
+    return (target == '}' || target == ']' || target == ')');
 }
 
 //проверяем строку
-bool check() {
-    char temp = '\0';
+bool check(string line) {
     myStack stack;
-    while (temp != '\r') {
-        cin >> temp;
-        cout << "a";
+    int i = 0;
+    for (i = 0; i < line.length(); i++) {
+        char temp = line[i];
         if (open_bracket(temp)) {
             stack.push(temp);
         }
         if (close_bracket(temp)) {
-            if ((temp - stack.top() == 1) || (temp - stack.top() == 2)) {
-                char d = stack.pop();
-                continue;
+            if (stack.is_empty()) {
+                return false;
+            }
+            if ((temp == '}' && stack.top() == '{')
+            || (temp == ']' && stack.top() == '[')
+            || (temp == ')' && stack.top() == '(')) {
+            char d = stack.pop();
+        }
             }
             else {
                 return false;
             }
         }
-
-    }
-    if (stack.is_empty()) {
-        return true;
-    }
-    else return false;
+    return stack.is_empty();
 }
 
 int main() {
     cout << "Enter your expression" << endl;
-    if (check()) {
+    string check_line;
+    cin.clear();
+    cin.sync();
+    getline(cin, check_line);
+    if (check(check_line)) {
         cout << "Right!" << endl;
     }
     else cout << "Wrong!" << endl;
